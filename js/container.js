dd304a028fd6943fb1a6c812f0cf2ea87074d7e6
Rick Koike <mail@ssig33.com>
2017-09-19 18:45:05 +0900

    Update container.js

--- js/container.js
+++ js/container.js
@@ -1,3 +1,4 @@
+// container の役目は殆ど魔術みたいなかんじ。
 import React from 'react'
 import { connect } from 'react-redux'
 
@@ -5,10 +6,13 @@ import Top from './component'
 
 import action from './action'
 
+
+// reducer が吐く state を全部 React に渡したいわけじゃない って時はここ使って編集しましょう。
 const mapStateToProps = (state)=>{
   return state
 }
 
+// 基本的にここに action を dispatch するの書けばいいだけ
 const mapDispatchToProps = (dispatch)=>{
   return {
     load: ()=> { return dispatch(action.load()); },
